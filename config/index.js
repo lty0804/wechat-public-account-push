0100f326bb494bd5cc4bf79ab9ae6e4aef8d4dc0
lty0804 <112307530+lty0804@users.noreply.github.com>
2022-09-16 16:35:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -179,11 +179,9 @@ export const config = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "*生日", "name": "老婆", "year": "2003", "date": "09-09", isShowAge: true},
+    {"type": "节日", "name": "纪念日", "year": "2022", "date": "08-04"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
     // 你可以不断按格式往下增加
     // ...
   ],
